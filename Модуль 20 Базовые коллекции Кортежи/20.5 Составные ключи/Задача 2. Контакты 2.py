bd6c08f1d9e5a5641286ca1b071352e3849fb9b9
Dmitriy Derzhavin <derjawindima83@gmail.com>
2022-06-24 00:26:53 +0300

    sixth-commit

---
+++ Модуль 20 Базовые коллекции Кортежи/20.5 Составные ключи/Задача 2. Контакты 2.py
@@ -0,0 +1,6 @@
+phonebook = dict()
+string = input('Введите имя, фамилию и номер: ').split()
+
+while string != 'end':
+    string = input('Введите имя, фамилию и номер: ').split()
+    print(string)
