66af983fb7d318745d207ff7bb128eaad4aa2b54
Dmitriy Derzhavin <derjawindima83@gmail.com>
2022-06-24 16:16:20 +0300

    seventh-commit

--- Модуль 20 Базовые коллекции Кортежи/20.5 Составные ключи/Задача 2. Контакты 2.py
+++ Модуль 20 Базовые коллекции Кортежи/20.5 Составные ключи/Задача 2. Контакты 2.py
@@ -1,6 +1,13 @@
 phonebook = dict()
-string = input('Введите имя, фамилию и номер: ').split()
 
-while string != 'end':
+while True:
     string = input('Введите имя, фамилию и номер: ').split()
-    print(string)
+    if string == ['end']:
+        break
+    elif (string[0], string[1]) in phonebook:
+        print('\nДанный контакт уже есть в телефонной книге')
+    else:
+        phonebook[(string[0], string[1])] = int(string[2])
+
+    for i_person in phonebook:
+        print('{0} {1} — {2}'.format(i_person[0], i_person[1], phonebook[i_person]))
